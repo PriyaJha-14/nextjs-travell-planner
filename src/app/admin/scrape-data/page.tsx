b618c05fdd7ffaf48dc67f9c9026cfcf9297455c
PriyaJha-14 <149561587+PriyaJha-14@users.noreply.github.com>
2025-07-26 22:04:16 +0530

    Making_scrapedata_page

---
+++ src/app/admin/scrape-data/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ScrapeData = () => {
+    return <section className="m-10grid grid-cols-3 gap-5">
+        
+
+    </section>;
+        
+    
+}
+
+export default ScrapeData;
---
+++ src/app/api/admin/create-job/route.ts
@@ -0,0 +1,15 @@
+
+
+
+
+export async function POST(request:Request) {
+    try {
+
+    }catch(error){
+        return NextResponse.json(
+            { message: "An unexpected error occurred." },
+            { status: 500 }
+        );
+
+    }
+}
