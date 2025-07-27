11ddb125de11e443988a90f17f4d86abba04e920
PriyaJha-14 <149561587+PriyaJha-14@users.noreply.github.com>
2025-07-27 21:36:35 +0530

    Update page.tsx

--- src/app/admin/scrape-data/page.tsx
+++ src/app/admin/scrape-data/page.tsx
@@ -1,12 +1,28 @@
+"use client";
 import React from "react";
+import { Card, CardFooter } from "@heroui/react";
+import { CardBody } from "@heroui/react";
+import {Tabs, Tab} from "@heroui/react";
+
 
 const ScrapeData = () => {
-    return <section className="m-10grid grid-cols-3 gap-5">
-        
+    return (
+        <section className="m-10 grid grid-cols-3 gap-5">
+            <Card className="col-span-2">
+                <CardBody>
+                    <Tabs>
+                        <Tab key="location" title="Location">
+                            
+                        </Tab>
+                    </Tabs>
+                </CardBody>
+            </Card>
+        </section>
+    );
+};
 
-    </section>;
-        
-    
-}
+
+
 
 export default ScrapeData;
+
