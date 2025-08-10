b772dbde5a6fe794bc758db7c9b0fc05f6e147bd
PriyaJha-14 <149561587+PriyaJha-14@users.noreply.github.com>
2025-08-10 19:17:30 +0530

    login issue resolved

--- src/utils/api-routes.ts
+++ src/utils/api-routes.ts
@@ -2,7 +2,7 @@
 const baseRoute = `${process.env.NEXT_PUBLIC_DOMAIN}/api`;
 
 export const ADMIN_API_ROUTES = {
-    LOGIN: `${baseRoute}/admin/login`,
+    LOGIN: `${baseRoute}/api/admin/login`,
     CREATE_JOB: `${baseRoute}/admin/create-job`,
     JOB_DETAILS: `${baseRoute}/admin/jobs`, // Assuming this is the correct URL
 };
