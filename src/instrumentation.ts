4d589701ee5fc441fdb334cae51d23216ea0ecc3
PriyaJha-14 <149561587+PriyaJha-14@users.noreply.github.com>
2025-08-15 17:10:19 +0530

    nothing happened

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -10,7 +10,7 @@ export const register = async () => {
     const { jobsQueue } = await import("@/lib/queue");
     const puppeteer = await import("puppeteer");
     const BROWSER_WS = "wss://brd-customer-hl_9a1ef175-zone-smartscrape:ezfhjjsqay1y@brd.superproxy.io:9222";
-
+    
     new Worker("jobsQueue", async (job) => {
       let browser: undefined | Browser = undefined;
       try {
@@ -19,11 +19,39 @@ export const register = async () => {
         });
         const page = await browser.newPage();
         console.log("Connected! Navigating to " + job.data.url);
+        await page.goto(job.data.url, { timeout: 20000 });
+        console.log("Navigated! Scraping page content...");
+        const html = await page.content();
+        console.log(html);
+
+
         if (job.data.jobType.type === "location") {
-          await page.goto(job.data.url, { timeout: 60000 });
-          console.log("Navigated! Scraping page content...");
+          // await page.goto(job.data.url, { timeout: 10000 });
+          // console.log("Navigated! Scraping page content...");
+          await page.waitForSelector(".packages-container", { timeout: 30000 });
           const packages = await startLocationScraping(page);
-          console.log({ packages });
+          await prisma.jobs.update({
+            where: { id: job.data.update },
+            data: { isComplete: true, status: "complete"},
+          });
+          for (const pkg of packages) {
+            const jobCreated = await prisma.jobs.findFirst({
+              where: {
+                url: `https://packages.yatra.com/holidays/intl/details.htm?packageId=${pkg?.id}`,
+              },
+            });
+            if (!jobCreated) {
+              const job = await prisma.jobs.create({
+                data: {
+                  url: `https://packages.yatra.com/holidays/intl/details.htm?packageId=${pkg?.id}`,
+                  jobType: { type: "package" },
+                },
+              });
+              jobsQueue.add("package", { ...job, packageDetails: pkg});
+            }
+          }
+        } else if (job.data.jobType.type === "package") {
+          console.log(job.data);
         }
       } catch (error) {
         console.log(error);
@@ -44,7 +72,6 @@ export const register = async () => {
       });
   }
 };
-
 
 
 
